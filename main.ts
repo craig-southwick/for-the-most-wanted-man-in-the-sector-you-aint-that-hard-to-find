509e099bac370ffa71204b330f28c95346d01816
craig-southwick <112982273+craig-southwick@users.noreply.github.com>
2022-09-06 14:14:23 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    raynor += 1
+    basic.showString("Raynor")
+    basic.showNumber(raynor)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    tycus += 1
+    basic.showString("tycus")
+    basic.showNumber(tycus)
+    basic.clearScreen()
+})
+let raynor = 0
+let tycus = 0
+tycus = 0
+raynor = 0
